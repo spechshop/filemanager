6baa6d805d5e276e157954c3d36c7c9e246d731b
berzersks <101850677+berzersks@users.noreply.github.com>
2026-05-08 06:16:59 -0400

    Fix chmod command to add execute permission for php

--- installer.sh
+++ installer.sh
@@ -7,7 +7,7 @@ npm rebuild
 npm install
 git clone https://github.com/spechshop/libspech
 wget https://github.com/spechshop/pcg729/releases/download/PCG729/php
-chmod -x php
+chmod +x php
 cp php /usr/local/bin
 export COMPOSER_ALLOW_SUPERUSER=1
 ./composer install
